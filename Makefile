d322af22ce4fcaef108dc447282770c8cc3f5c95
Max <agnusmaximus@berkeley.edu>
2016-08-21 03:40:39 -0700

    Update

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
-FLAGS=-Ofast -std=c++11 -omp
+FLAGS=-Ofast -std=c++11 -fopenmp
+CC=clang-omp++
 
 ising:
 	rm -f ising_bin
-	g++-5 $(FLAGS) src/GibbsSamplingIsing.cpp -o ising_bin
+	$(CC) $(FLAGS) src/GibbsSamplingIsing.cpp -o ising_bin
 	./ising_bin
--- src/GibbsSamplingIsing.cpp
+++ src/GibbsSamplingIsing.cpp
@@ -1,22 +1,49 @@
 // Gibbs sampling on a synthetic Ising model.
 // See http://arxiv.org/pdf/1602.07415v2.pdf for details.
+// As in the paper,  assume prior weights B_x is 0.
 #include <iostream>
 #include <omp.h>
 #include <map>
 #include <vector>
 #include <limits>
+#include <math.h>
 
 using namespace std;
+
+#define HOGWILD 1
+#define N_THREADS 1
 
 #define N 1000                // Number of vertices
 #define DELTA 3               // Maximum degree of vertices
 #define BETA .2               // Inverse temperature
-#define PRIOR_WEIGHTS 0       // Prior weights Ex
+#define N_ITERATIONS 10000
 
 #define MAX_EDGES (N*N)
 #define MAX_EDGE_INSERTION_TRIES (N*N)
 
 typedef map<int, vector<int> > Graph;
+
+// Note that access pattern has form:
+// [thread][batch][state index].
+typedef vector<vector<vector<int> > > AccessPattern;
+
+void PrintState(vector<int> &state) {
+    // For conciseness, print -1 as 0.
+    string state_string = "";
+    for (int i = 0; i < state.size(); i++) {
+	if (state[i] == 1) {
+	    state_string += "1";
+	}
+	else if (state[i] == -1) {
+	    state_string += "0";
+	}
+	else {
+	    cout << "Something went wrong..." << endl;
+	    exit(0);
+	}
+    }
+    cout << state_string << endl;
+}
 
 void PrintGraph(Graph &g) {
     for (int i = 0; i < N; i++) {
@@ -47,7 +74,7 @@ void PrintGraphStatistics(Graph &g) {
 
 // Initialize an empty graph g with a
 // synthetic ising graph.
-Graph GenerateIsingModelGraph() {
+Graph GenerateRandomIsingModelGraph() {
     Graph g;
 
     // Initialize vertices.
@@ -79,7 +106,83 @@ Graph GenerateIsingModelGraph() {
     return g;
 }
 
+vector<int> GenerateIsingState() {
+    vector<int> state(N);
+    for (int i = 0; i < N; i++) {
+	if (rand() % 2 == 0) {
+	    state[i] = 1;
+	}
+	else {
+	    state[i] = -1;
+	}
+    }
+    return state;
+}
+
+int PartitionDatapointsForHogwild(Graph &g, vector<int> &state, AccessPattern &pattern) {
+    pattern.resize(N_THREADS);
+    int n_datapoints_per_thread = N / N_THREADS;
+    for (int thread = 0; thread < N_THREADS; thread++) {
+	pattern[thread].resize(1);
+	int start = n_datapoints_per_thread * thread;
+	int end = n_datapoints_per_thread * (thread+1);
+	if (thread == N_THREADS-1) end = N;
+	for (int index = start; index < end; index++) {
+	    pattern[thread][0].push_back(index);
+	}
+    }
+    return 1; // 1 batch for hogwild.
+}
+
+void UpdateState(Graph &g, vector<int> &state, int index) {
+    int product_with_1 = 0;
+    int product_with_neg_1 = 0;
+    for (int i = 0; i < g[index].size(); i++) {
+	product_with_1 += state[g[index][i]];
+	product_with_neg_1 += state[g[index][i]] * -1;
+    }
+
+    double p1 = exp(BETA * (double)product_with_1);
+    double p2 = exp(BETA * (double)product_with_neg_1);
+    double prob_1 = p1 / (p1 + p2);
+    double selection = ((double)rand() / (RAND_MAX));
+    if (selection <= prob_1) {
+	state[index] = 1;
+    }
+    else {
+	state[index] = -1;
+    }
+}
+
 int main(int argc, char *argv[]) {
-    Graph g = GenerateIsingModelGraph();
+    omp_set_num_threads(N_THREADS);
+
+    // Generate graph.
+    Graph g = GenerateRandomIsingModelGraph();
     PrintGraphStatistics(g);
+
+    // Generate variables.
+    vector<int> state = GenerateIsingState();
+
+    // Access pattern partitions.
+    // Of form [thread][batch][state to update].
+    // Note that for hogwild, there will only be one batch
+    AccessPattern access_pattern;
+    int n_batches = 0;
+    if (HOGWILD) {
+	n_batches = PartitionDatapointsForHogwild(g, state, access_pattern);
+    }
+
+    for (int iter = 0; iter < N_ITERATIONS; iter++) {
+#pragma omp parallel for num_threads(N_THREADS)
+	for (int thread = 0; thread < N_THREADS; thread++) {
+	    for (int batch = 0; batch < n_batches; batch++) {
+		for (int to_update = 0; to_update < access_pattern[thread][batch].size(); to_update++) {
+		    int index_to_update = access_pattern[thread][batch][to_update];
+		    UpdateState(g, state, index_to_update);
+		}
+	    }
+	}
+	//PrintState(state);
+    }
 }
